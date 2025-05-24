f4c9d2edd29d1193fa2880ede9d819efd1f363cb
LapTop_Bogdan <bogdanvoinov90@gmail.com>
2025-05-24 19:27:05 +0300

    fix db

--- DataBaseManager/__init__.py
+++ DataBaseManager/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 from api.utils.variable_environment import VarEnv
-from DataBaseManager.models import Category, MusicMeta
+from DataBaseManager.models import Category, MusicMeta, Base
 from typing import List, Type, TypeVar, Optional
 
 
@@ -16,10 +16,10 @@ class DataBaseManager:
             session.commit()
             session.close()
 
-    def select(self, model: Type[TypeVar],
+    def select(self, model: Type[Base],
                filter_condition: Optional[dict] = None,
                limit: Optional[int] = None,
-               offset: Optional[int] = None) -> List[TypeVar]:
+               offset: Optional[int] = None) -> List[Base]:
         session = self.get_session()
         try:
             query = session.query(model)
@@ -38,10 +38,15 @@ class DataBaseManager:
             print(e)
         finally:
             session.close()
-            raise Exception("DatabaseSelectError")
+
+    def select_music_by_id(self, id):
+        return self.select(MusicMeta, filter_condition={"music_id": id}, limit=1)
 
     def get_session(self):
-        return self.engine.connect()
+        Session = sessionmaker(bind=self.engine)
+        return Session()
 
 
 db = DataBaseManager()
+if __name__ == "__main__":
+    print(db.select_music_by_id(0))
--- DataBaseManager/models.py
+++ DataBaseManager/models.py
@@ -1,9 +1,17 @@
 from sqlalchemy import Column, Integer, String, Date, ForeignKey, DateTime
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import declarative_base, relationship, DeclarativeBase
 from sqlalchemy.dialects.postgresql import JSON, TSVECTOR
 from datetime import datetime
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    def __repr__(self):
+        fields = ', '.join(f"{k}={getattr(self, k)!r}" for k in self.__mapper__.columns.keys())
+        return f"{self.__class__.__name__}({fields})"
+
+    def __str__(self):
+        return self.__repr__()
+
 
 class Category(Base):
     __tablename__ = 'category'
@@ -12,11 +20,12 @@ class Category(Base):
 
     owner_music_meta = relationship("MusicMeta", back_populates="owner_category")
 
+
 class MusicMeta(Base):
     __tablename__ = 'music_meta'
     music_id: int = Column(Integer, primary_key=True)
     name: str = Column(String)
-    music_category: int = Column(Integer)
+    music_category: int = Column(Integer, ForeignKey('category.category_id'))
     text_music: str = Column(String)
     url: str = Column(String)
     search_vector = Column(TSVECTOR)
